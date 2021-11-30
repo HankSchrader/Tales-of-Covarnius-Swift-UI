6ca60c8a2b7f837a808a12203b2dc56a130eb952
Erik Mikac <mikacew@gmail.com>
2021-11-29 19:58:47 -0500

    Initial Commit

---
+++ Shared/Tales_of_Covarnius_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tales_of_Covarnius_Swift_UIApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Tales_of_Covarnius_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
