16863dd062906a727e61f5275633b5f6ab21e185
Erik Mikac <mikacew@gmail.com>
2021-12-04 08:16:14 -0500

    WIP. Working on side-nav.

--- Shared/utilities/PageView.swift
+++ Shared/utilities/PageView.swift
@@ -6,31 +6,3 @@
 //
 
 import SwiftUI
-func getPageView(mainText: String,
-                             firstChoice: AnyView,
-                 decision1: String = Constants.ContinuePhrase,
-                             secondChoice: AnyView = AnyView(Text("s")) ,
-                             decision2: String = "") -> some View {
-    let isIPad = UIDevice.current.userInterfaceIdiom == .pad
-    return VStack {
-        Text(mainText)
-            .fontWeight(.light)
-            .if(isIPad) {
-                view in
-                view.font(.system(size: CGFloat(Constants.ipadFontSize)))
-            }.padding()
-            Spacer()
-        
-        NavigationLink(destination: firstChoice.navigationBarBackButtonHidden(decision2 != "")) {
-                   Text(decision1)
-               }
-        .padding()
-        if(decision2 != "") {
-            NavigationLink(destination: secondChoice.navigationBarBackButtonHidden(true)) {
-                       Text(decision2)
-                    .padding()
-            }
-        }
-    }
-    .padding(.bottom)
-    }
---
+++ Shared/utilities/RootViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RootViewModel.swift
+//  Tales-of-Covarnius-Swift-UI (iOS)
+//
+//  Created by Erik Mikac on 12/4/21.
+//
+
+import SwiftUI
+
+class RootViewModel: ObservableObject {
+    @Published var showMenu = false
+}
