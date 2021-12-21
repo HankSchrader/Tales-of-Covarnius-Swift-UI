a966ed10a72ab63959364346f1206e8a7cf6bcc5
Erik Mikac <mikacew@gmail.com>
2021-12-20 21:06:14 -0500

    Fixed bug where Part_1_Hapalstance pointed to itself instead of the next page.

--- Shared/View/Intro/See What's In The Sky/Part-2-See-Whats-In-The-Sky.swift
+++ Shared/View/Intro/See What's In The Sky/Part-2-See-Whats-In-The-Sky.swift
@@ -12,7 +12,7 @@ struct Part_2_See_Whats_In_The_Sky: View {
     static let PageName = "Part_2_See_What_In_The_Sky"
     var body: some View {
         let text =
-        "Luna’s head tilts to the left, she is staring at the object as well. Could the two of you really be staring at a UFO? It doesn’t seem possible, you don’t even believe in aliens!\n\nBut the odd green creature in the UFO makes you think otherwise. You hear the loud his of decompressing of air. The top of what appears to be an alien spaceship opens.\n\nYup, definitely an alien!"
+        "Luna’s head tilts to the left, she's staring at the object too. Could the two of you really be staring at a UFO? It doesn’t seem possible, you don’t even believe in aliens!\n\nBut the odd green creature in the UFO makes you think otherwise. You hear a loud hiss of decompressing air. The top of what appears to be an alien spaceship opens.\n\nYup, definitely an alien!"
         let decision1 = Constants.ContinuePhrase
         let firstChoicePageName = Part_3_See_Whats_In_The_Sky.PageName
         let storyView: StoryPayload = StoryPayload(text: text, decision1: decision1, firstChoicePageName: firstChoicePageName)
