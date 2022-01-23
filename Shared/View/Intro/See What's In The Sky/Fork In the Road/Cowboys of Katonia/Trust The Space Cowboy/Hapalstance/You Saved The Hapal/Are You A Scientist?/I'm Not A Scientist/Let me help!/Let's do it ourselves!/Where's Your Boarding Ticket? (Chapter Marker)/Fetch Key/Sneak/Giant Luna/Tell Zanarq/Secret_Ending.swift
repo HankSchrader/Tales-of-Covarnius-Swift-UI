a03c50ca2e11e41ffb1188c59d1f911d054a418b
Erik Mikac <mikacew@gmail.com>
2022-01-23 15:41:40 -0500

    Fixed but in Part_2_Feed_Dogs. Added pictures

---
+++ Shared/View/Intro/See What's In The Sky/Fork In the Road/Cowboys of Katonia/Trust The Space Cowboy/Hapalstance/You Saved The Hapal/Are You A Scientist?/I'm Not A Scientist/Let me help!/Let's do it ourselves!/Where's Your Boarding Ticket? (Chapter Marker)/Fetch Key/Sneak/Giant Luna/Tell Zanarq/Secret_Ending.swift
@@ -0,0 +1,20 @@
+//
+//  Secret_Ending.swift
+//  Tales-of-Covarnius-Swift-UI (iOS)
+//
+//  Created by Erik Mikac on 1/23/22.
+//
+
+import SwiftUI
+
+struct Secret_Ending: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Secret_Ending_Previews: PreviewProvider {
+    static var previews: some View {
+        Secret_Ending()
+    }
+}
