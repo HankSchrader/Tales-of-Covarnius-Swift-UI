6eaaa68f6b942837edf932e791052292825c794f
Erik Mikac <mikacew@gmail.com>
2022-05-01 10:20:12 -0400

    Portrait only.

--- Shared/View/Intro/See What's In The Sky/Fork In the Road/Cowboys of Katonia/Trust The Space Cowboy/Hapalstance/You Saved The Hapal/Are You A Scientist?/I'm Not A Scientist/Let me help!/Let's do it ourselves!/Diplomatic Immunity (Chapter Markter)/Sneak Into House/They_Are_Vicious/Part_5_They_Are_Vicious.swift
+++ Shared/View/Intro/See What's In The Sky/Fork In the Road/Cowboys of Katonia/Trust The Space Cowboy/Hapalstance/You Saved The Hapal/Are You A Scientist?/I'm Not A Scientist/Let me help!/Let's do it ourselves!/Diplomatic Immunity (Chapter Markter)/Sneak Into House/They_Are_Vicious/Part_5_They_Are_Vicious.swift
@@ -16,7 +16,7 @@ struct Part_5_They_Are_Vicious: View {
         
         let decision1 = Constants.ContinuePhrase
         let firstChoicePageName = Part_2_Council.PageName
-        let storyView: StoryPayload = StoryPayload(text: text, decision1: decision1, firstChoicePageName: firstChoicePageName)
+        let storyView: StoryPayload = StoryPayload(text: text, image: "Outside Home", decision1: decision1, firstChoicePageName: firstChoicePageName)
         return DisplayView(showMenu: self.$showMenu, view: storyView)
     }
 }
